38579e89010af4699e80eb56959502db8bc10019
StivenLeon23 <stivenleon.23@gmail.com>
2023-09-28 21:55:57 -0500

    se realiza back de autenticacion

---
+++ server/routers/validate-token.js
@@ -0,0 +1,16 @@
+const jwt = require ('jsonwebtoken')
+
+const verifyToken = (req,res,next) =>{
+    const token = req.header ('auth-token')
+    if(!token) return res.status(401).json({error:'Acceso Denegado'})
+
+    try{
+        const verificar = jwt.verify(token, process.env.TOKEN_SECRET)
+        req.user = verificar
+        next() 
+    }catch (error){
+        res.status(400).json({error: 'token no es válido'})
+    }
+}
+
+module.exports = verifyToken
